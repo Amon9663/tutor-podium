5be90bf7c668702926b245ac6743348348809f9b
Akono Brathwaite <akonobrathwaite@gmail.com>
2020-05-29 12:26:14 -0400

    Added error handling for http requests

--- tutorgetip/plugin.py
+++ tutorgetip/plugin.py
@@ -8,28 +8,37 @@ HERE = os.path.abspath(os.path.dirname(__file__))
 
 templates = os.path.join(HERE, "templates")
 
-# Send request to AWS to get IP address
-response = requests.get('http://localhost:3000/')
-# TODO: Find way to validate IP ADDRESS
-IP_ADDRESS = response.text
-# TODO: Remove
-print("getIP plugin is working")
-config = {
+# Handle Errors when Sending Query to server --> All `tutor` commands run this program regardless of whether the plugin is enabled or disabled
+try:
+    # Send request to AWS to get IP address
+    # TODO: Change URL
+    response = requests.get('http://localhost:3000/')
+
+    # TODO: Find way to validate IP ADDRESS
+    IP_ADDRESS = response.text
+
+    # TODO: Remove
+    print("getIP plugin is working")
+
+    config = {
     # Run command `tutor config save --set MYPLUGIN_AWS_IP_LOOKUP=desiredValue` to set value (True or False)
     "add": {
         "AWS_HOST_IP": IP_ADDRESS,
         "AWS_IP_LOOKUP": True
     },
 
-    # Does not automatically append plugin name to key name in config.yml as per docs (Potential bug) 
+    # Does not automatically append plugin name to key name in config.yml as per docs (Potential bug)
     # "set":{
     #     "AWS_IP_LOOKUP": False
     # },
     # Does not automatically append plugin name to key name in config.yml as per docs (Potential bug)
     # "defaults": {
-    #     "MYPLUGIN_AWS_IP_LOOKUP": True
+    #     "AWS_IP_LOOKUP": True
     # }
-}
+    }
+except requests.exceptions.RequestException as e:
+    print(f"Error getting IP Address: {e}")
+
 
 hooks = {}
 
